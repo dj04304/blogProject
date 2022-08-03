b102b8169c53ffb2e2245ffa22bee9fe1df7f90c
dj04304 <dj043045@gmail.com>
2022-08-03 12:41:05 +0900

    blogproject git test

---
+++ blogProject-20220802/src/HelloWorld.java
@@ -0,0 +1,11 @@
+
+//keyMap = 인텔리 J = shift + f10
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("테스트");
+		System.out.println("테스트");
+		System.out.println("테스트");
+		System.out.println("테스트");
+		System.out.println("테스트");
+	}
+}
